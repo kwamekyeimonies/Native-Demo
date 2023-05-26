c8527fb0319b7b4a68e0eb7773501622493a77fa
kwamekyeimonies <kwamekyeimonies@gmail.com>
2023-05-26 17:55:25 +0000

    dev

--- screens/Homescreen.js
+++ screens/Homescreen.js
@@ -1,7 +1,19 @@
-import { View, Text, SafeAreaView, Image } from 'react-native'
+import { View, Text, SafeAreaView, Image, TextInput } from 'react-native'
 import React, { useLayoutEffect } from 'react'
 import { useNavigation } from '@react-navigation/native'
 import myImg from "../assets/profile.jpg"
+import
+{
+    UserIcon,
+    ChevronDownIcon,
+    // SearchIcon,
+    AdjustmentsHorizontalIcon,
+    AdjustmentsVerticalIcon,
+    MagnifyingGlassCircleIcon,
+    UserCircleIcon,
+    MagnifyingGlassIcon,
+    // AdjustmentsIcon,
+} from "react-native-heroicons/outline"
 
 const Homescreen = () =>
 {
@@ -16,21 +28,38 @@ const Homescreen = () =>
     }, [])
 
     return (
-        <SafeAreaView>
-            {/* Header */}
-            <View className="flex-row pb-3 items-center mx-4 space-x-2">
-                <Image
-                    source={myImg}
-                    // source={{
-                    //     uri: 'https://links.papareact.com/wru'
-                    // }}
-                    className="h-7 w-7 bg-gray-300 p-4 rounded-full"
-                />
-                <View>
-                    <Text className="font-bold text-gray-400 text-xs">Deliver Now!</Text>
-                    <Text className="font-bold text-xl">Current Location</Text>
+        <SafeAreaView className="bg-white pt-5">
+            <Text className="text-red-500">
+                {/* Header */}
+                <View className="flex-row pb-3 items-center mx-4 space-x-2 px-4">
+                    <Image
+                        source={{
+                            uri: "https://links.papareact.com/wru",
+                        }}
+                        className="h-7 w-7 bg-gray-400 rounded-full"
+                    />
+
+                    <View className="flex-1">
+                        <Text className="font-bold text-gray-400 text-xs">
+                            Deliver Now!
+                        </Text>
+                        <Text className="font-bold text-xl">
+                            Current Location
+                            <ChevronDownIcon size={20} color="#00CCBB" />
+                        </Text>
+                    </View>
+
+                    <UserIcon size={35} color="#00CCBB" />
                 </View>
-            </View>
+
+                <View className="flex-row items-center space-x-2 pb-2 mx-4">
+                    <View className="flex-row space-x-2 bg-gray-200">
+                        <MagnifyingGlassIcon color="gray" size={20} />
+                        <TextInput placeholder='Restaurants and cuisines' keyboardType='default' />
+                    </View>
+                    <AdjustmentsVerticalIcon color="#00CCBB" />
+                </View>
+            </Text>
         </SafeAreaView>
     )
 }
