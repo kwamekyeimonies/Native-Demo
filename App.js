66340eb468259978f04cee84582fdd0c0b09c020
kwamekyeimonies <kwamekyeimonies@gmail.com>
2023-05-26 03:25:56 +0000

    dev

--- App.js
+++ App.js
@@ -1,11 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, Linking, StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+
+export default function App()
+{
+  const [name, setName] = useState("Tenkorang Daniel");
+  const [session, setSession] = useState({ number: 6, title: "state" });
+  const [current, setCurrent] = useState(true);
+
+  const onClickNameChanger = () =>
+  {
+    setName("Kwame Daniel")
+    setSession({ number: 100, title: "Demo sessions" })
+    setCurrent(false)
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.text}>My name is {name}</Text>
+      <Text style={styles.text}>This session number is {session.number} and about {session.title} </Text>
+      <Text style={styles.text}>{
+        current ? "Current sesion" : "Next Session"
+      } </Text>
+      <Button title='Change Name' onPress={onClickNameChanger} />
+
     </View>
   );
 }
@@ -17,4 +36,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    color: "red",
+    fontSize: 20,
+    fontStyle: 'italic',
+  }
 });
