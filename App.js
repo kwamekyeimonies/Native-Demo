9a231d957a59fde2a1252c887a8cb695083d78d8
kwamekyeimonies <kwamekyeimonies@gmail.com>
2023-05-26 09:59:14 +0000

    deliveroo clone

--- App.js
+++ App.js
@@ -1,14 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Homescreen from './screens/Homescreen';
 
+
+const Stack = createNativeStackNavigator();
 
 export default function App()
 {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-green-500">Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name='Home' component={Homescreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
