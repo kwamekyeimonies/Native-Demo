779aabb1117de77c3f568054b5723cfb9d810671
kwamekyeimonies <kwamekyeimonies@gmail.com>
2023-05-26 09:45:48 +0000

    tailwind config fixed

--- App.js
+++ App.js
@@ -1,35 +1,14 @@
-import axios from 'axios';
 import { StatusBar } from 'expo-status-bar';
-import { useState } from 'react';
-import { Button, Linking, StyleSheet, Text, View } from 'react-native';
-
-const getPopularMovies = async () =>
-{
-  const response = await axios.get("https://api.themoviedb.org/3/movie/popular?api_key=ec5ec44bf5de4897b94300d5e5ca2abb")
-  console.log(JSON.stringify(response, null, 2));
-}
+import { Text, View } from 'react-native';
 
 
 export default function App()
 {
   return (
-    <View style={styles.container}>
-      <Text>Hello</Text>
-      <Button onPress={getPopularMovies} title='Check DB' />
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-green-500">Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    // color: "red",
-    fontSize: 20,
-    fontStyle: 'italic',
-  }
-});
