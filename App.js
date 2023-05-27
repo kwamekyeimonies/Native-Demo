f4207f046c699b73c2df87b1dc2b1d364f3e912a
kwamekyeimonies <kwamekyeimonies@gmail.com>
2023-05-27 02:44:14 +0000

    playground

--- App.js
+++ App.js
@@ -1,44 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
 import { Button, Linking, StyleSheet, Text, View } from 'react-native';
+import styles from './style';
 
 
 export default function App()
 {
-  const [name, setName] = useState("Tenkorang Daniel");
-  const [session, setSession] = useState({ number: 6, title: "state" });
-  const [current, setCurrent] = useState(true);
-
-  const onClickNameChanger = () =>
-  {
-    setName("Kwame Daniel")
-    setSession({ number: 100, title: "Demo sessions" })
-    setCurrent(false)
-  }
 
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>My name is {name}</Text>
-      <Text style={styles.text}>This session number is {session.number} and about {session.title} </Text>
-      <Text style={styles.text}>{
-        current ? "Current sesion" : "Next Session"
-      } </Text>
-      <Button title='Change Name' onPress={onClickNameChanger} />
+      <View style={styles.view1}>
+        <Text style={styles.text}>1</Text>
+      </View>
 
+      <View style={styles.view2}>
+        <Text style={styles.text}>2</Text>
+      </View>
+
+      <View style={styles.view3}>
+        <Text style={styles.text}>3</Text>
+      </View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    color: "red",
-    fontSize: 20,
-    fontStyle: 'italic',
-  }
-});
---
+++ style.js
@@ -0,0 +1,37 @@
+import { Button, Linking, StyleSheet, Text, View } from 'react-native';
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+        flexDirection: 'space-evenly'
+    },
+    text: {
+        color: "#00CCBB",
+        fontSize: 20,
+        fontStyle: 'italic',
+    },
+    view1: {
+        width: 100,
+        height: 100,
+        backgroundColor: "red",
+        alignItems: 'center'
+    },
+    view2: {
+        width: 100,
+        height: 100,
+        backgroundColor: "yellow",
+        alignItems: 'center'
+    },
+    view3: {
+        width: 100,
+        height: 100,
+        backgroundColor: "green",
+        alignItems: 'center'
+    }
+});
+
+
+export default styles;
